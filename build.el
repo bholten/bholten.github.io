6eca375f07784379b4cb0db940c0689911a23b1b
Brennan Holten <bjh@debian>
2022-07-23 01:14:25 -0500

    Testing syntax highlighting and themes

--- build.el
+++ build.el
@@ -17,10 +17,10 @@
 (require 'ox-publish)
 
 ;; Customize the HTML output
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+(setq org-html-validation-link nil
+      org-html-head-include-scripts t
+      org-html-head-include-default-style nil
+      org-html-head nil)
 
 ;; Define the publishing project
 (setq org-publish-project-alist
