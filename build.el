659add71dd923cbc85f04a2bd5e7389005d7b5d8
Brennan Holten <bjh@debian>
2022-07-23 02:17:29 -0500

    Added css, auto-sitemap as index

--- build.el
+++ build.el
@@ -18,23 +18,34 @@
 
 ;; Customize the HTML output
 (setq org-html-validation-link nil
-      org-html-head-include-scripts t
+      org-html-head-include-scripts nil
       org-html-head-include-default-style nil
-      org-html-head nil)
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://bholten.github.io/css/retro.css\"/>")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
-      (list
-       (list "org-site:main"
-             :recursive t
-             :base-directory "./content"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./.public"
-             :with-author nil
-             :with-creator nil
-             :with-toc nil
-             :section-numbers nil
-             :time-stamp-file nil)))
+      '(("main"
+         :auto-sitemap t
+         :recursive t
+         :base-directory "./content"
+         :base-extension "org"
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "./.public"
+         :sitemap-filename "index.org"
+         :sitemap-style list
+         :sitemap-title "brennan.holten"
+         :with-author nil
+         :with-creator nil
+         :with-toc nil
+         :section-numbers nil
+         :time-stamp-file nil)
+        ("css"
+         :base-directory "css/"
+         :base-extension "css"
+         :publishing-directory ".public/css"
+         :publishing-function org-publish-attachment
+         :recursive t)
+        ("all" :components ("css" "main"))))
 
 ;; Generate the site output
 (org-publish-all t)
