0cfda5d4c1b5415c2b67ab6dc367ea73bada4b68
Brennan Holten <bjh@debian>
2022-07-27 14:49:19 -0500

    Syntax highlighting with prism.js

--- build.el
+++ build.el
@@ -10,6 +10,38 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
+;; Override org-mode's src-block function.
+;; This is in order to give prism.js the proper tags.
+(eval-after-load "ox-publish"
+  '(defun org-html-src-block (src-block contents info)
+     "Transcode a SRC-BLOCK element from Org to HTML.
+CONTENTS holds the contents of the item.  INFO is a plist holding
+contextual information."
+     (if (org-export-read-attribute :attr_html src-block :textarea)
+         (org-html--textarea-block src-block)
+       (let ((lang (org-element-property :language src-block))
+             (caption (org-export-get-caption src-block))
+             (code (org-html-format-code src-block info))
+             (label (let ((lbl (and (org-element-property :name src-block)
+                                    (org-export-get-reference src-block info))))
+                      (if lbl (format " id=\"%s\"" lbl) ""))))
+         (if (not lang)
+             (format "<pre class=\"example\"%s>\n%s</pre>" label code)
+           (format
+            "<div class=\"org-src-container\">\n%s%s\n</div>"
+            (if (not caption)
+                ""
+              (format "<label class=\"org-src-name\">%s</label>"
+                      (org-export-data caption info)))
+            (format
+             ;; prism.js wants this:
+             ;; <pre class="... language-*"><code> CODE HERE </code></pre>
+             "\n<pre class=\"src src-%s language-%s\"%s><code>%s</code></pre>"
+             lang
+             lang
+             label
+             code)))))))
+
 ;; Install dependencies
 (package-install 'htmlize)
 
@@ -21,6 +53,8 @@
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
       org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://bholten.github.io/css/retro.css\"/>")
+
+(setq org-src-fontify-natively t)
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -45,7 +79,12 @@
          :publishing-directory ".public/css"
          :publishing-function org-publish-attachment
          :recursive t)
-        ("all" :components ("css" "main"))))
+        ("js"
+         :base-directory "js/"
+         :base-extension "js"
+         :publishing-directory ".public/js"
+         :publishing-function org-publish-attachment)
+        ("all" :components ("css" "js" "main"))))
 
 ;; Generate the site output
 (org-publish-all t)
